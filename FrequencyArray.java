f60dd55a78b03f9149da1524d40aaf16b6dd7adf
subhadipjava <pandasubhadip05@gmail.com>
2026-04-27 16:17:05 +0530

    Add files via upload

---
+++ FrequencyArray.java
@@ -0,0 +1,27 @@
+public class FrequencyArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 3, 4, 3, 2, 1};
+
+        boolean[] visited = new boolean[arr.length];
+
+        System.out.println("Element | Frequency");
+
+        for (int i = 0; i < arr.length; i++) {
+            if (visited[i] == true)
+                continue;
+
+            int count = 1;
+
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                    visited[j] = true;
+                }
+            }
+
+            System.out.println(arr[i] + "       | " + count);
+        }
+    }
+}
+    
+
